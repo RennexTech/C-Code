b172896218bb95528f3a0c67178c942c7c40dda0
Rennex <139177099+NixonSchool@users.noreply.github.com>
2025-05-26 22:26:14 +0300

    .

--- Beginner Topic 1/S03.Functions.Keywords/E003.Keywords2.c
+++ Beginner Topic 1/S03.Functions.Keywords/E003.Keywords2.c
@@ -16,8 +16,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(1);
     
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("1. auto\n");
     sleep(1);
@@ -49,8 +53,12 @@ int main()
     // The compiler already knows, unless you're in a 1980s PC.
     // In short auto is saying "this is temporary" but the compiler already knows your variables are all temporary, till main exits.
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("2. break\n");
     sleep(1);
@@ -59,8 +67,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("3. case\n");
     sleep(1);
@@ -71,8 +83,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("4. char\n");
     sleep(1);
@@ -85,19 +101,27 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("5. const\n");
     sleep(1);
     printf("--------------------------------------------\n");
-    printf("The 'const' keyword means 'hands off' — once you set a value, it’s locked. You can read it, vibe with it, print it, but you *can’t* change it. It’s like putting a sign that says ‘DO NOT TOUCH’ on a variable. Great for keeping things safe and avoiding dumb mistakes.\n");
+    printf("The 'const' keyword means 'hands off!😤' — once you set a value, it’s locked. You can read it, vibe with it, print it, but you *can’t* change it. It’s like putting a sign that says ‘DO NOT TOUCH’ on a variable. Great for keeping things safe and avoiding dumb mistakes.\n");
     printf("--------------------------------------------\n");
     printf(" 'const' is like giving your variable a permanent tattoo — once it's inked, you can't change it. It locks the value down so nobody (not even future you) can mess it up by accident unless you remove the keyword. Basically, it's the compiler's way of saying, 'Don't you dare try to edit this value, bro.'\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("6. continue\n");
     sleep(1);
@@ -107,8 +131,12 @@ int main()
     printf("Imagine you’re a teacher who wants to skip students scoring below 50 — so you can take them for a quick chat in the staffroom😂. Your C program reads each name, but when it spots a below 50, it skips printing their name. You go back to class with the printed names, those who don’t hear their name? Staffroom, baby. 😂😂\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("7. default\n");
     sleep(1);
@@ -118,8 +146,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("8. do\n");
     sleep(1);
@@ -130,8 +162,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("9. double\n");
     sleep(1);
@@ -141,8 +177,11 @@ int main()
     sleep(5);
 
 
+
     printf("\n");
     printf("\n");
+
+
 
     printf("10. else\n");
     sleep(1);
@@ -152,8 +191,11 @@ int main()
     sleep(5);
 
 
+
     printf("\n");
     printf("\n");
+
+
 
     printf("--------------------------------------------\n");
     printf("11. enum\n");
@@ -198,10 +240,14 @@ int main()
         return 0;
     }
 
-    // Atleast you have an idea now...
+    // Atleast you have an idea now... we'll use them later on.
+
+
 
     printf("\n");
     printf("\n");
+
+
 
     printf("--------------------------------------------\n");
     printf("12. extern\n");
@@ -215,8 +261,12 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+
     printf("\n");
     printf("\n");
+
+
 
     printf("13. float\n");
     sleep(1);
@@ -225,6 +275,8 @@ int main()
     printf("--------------------------------------------\n");
     sleep(5);
 
+
+    
     printf("\n");
     printf("\n");
 
